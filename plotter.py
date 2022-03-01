7d243faccc83b6c8041eb198f13e62de2e28cb87
Mast3r_waf1z <thomasmollerjensen@live.dk>
2022-03-01 14:18:09 +0100

    added more to plotter

--- plotter.py
+++ plotter.py
@@ -1,4 +1,4 @@
-from matplotlib.pyplot import plot, savefig
+from matplotlib.pyplot import plot, savefig, scatter
 import json
 import sys
 guild = sys.argv[1]
@@ -7,5 +7,6 @@ with open("leaderboard2.json") as file:
     jsonstring = json.loads(file.read())
 l = [jsonstring[guild][id] for id in jsonstring[guild]]
 l.sort(reverse=True)
-plot(l)
+#plot(l)
+scatter(range(len(l)), l)
 savefig("tmp.png")
